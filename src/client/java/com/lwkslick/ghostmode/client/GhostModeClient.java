82fabf6705cae460e4317e4393bee2d84c0e8b11
lwkSlick <phoenixgamer946@gmail.com>
2026-05-31 17:44:49 +0530

    1.0.0.9
    feat: add panic key (K) and GhostMode keybind category

--- src/client/java/com/lwkslick/ghostmode/client/GhostModeClient.java
+++ src/client/java/com/lwkslick/ghostmode/client/GhostModeClient.java
@@ -19,9 +19,11 @@ public class GhostModeClient implements ClientModInitializer {
 	public static final String MOD_ID = "ghostmode";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static KeyBinding openConfigKey;
+	public static KeyBinding panicKey;
 
 	@Override
 	public void onInitializeClient() {
+		KeyBinding.Category ghostCategory = new KeyBinding.Category(net.minecraft.util.Identifier.of("ghostmode", "keycategory"));
 		GhostModeConfig.load();
 		ChatSentinel.register();
 		WatermarkHud.register();
@@ -31,7 +33,14 @@ public class GhostModeClient implements ClientModInitializer {
 				"key.ghostmode.open_config",
 				InputUtil.Type.KEYSYM,
 				GLFW.GLFW_KEY_J,
-				KeyBinding.Category.MISC
+				ghostCategory
+		));
+
+		panicKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.ghostmode.panic",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_K,
+				ghostCategory
 		));
 
 		net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
@@ -51,7 +60,23 @@ public class GhostModeClient implements ClientModInitializer {
 					client.setScreen(GhostModeConfigScreen.create(null));
 				}
 			}
+			while (panicKey.wasPressed()) {
+				GhostModeConfig.Profile p = GhostModeConfig.get().getActiveProfile();
+				p.streamModeEnabled = true;
+				p.sentinelEnabled = true;
+				p.sentinelChat = true;
+				p.sentinelTabList = true;
+				p.sentinelNametags = true;
+				p.sentinelScoreboard = true;
+				p.sentinelF3 = true;
+				p.maskOwnTabEntry = true;
+				p.hideServerIp = true;
+				p.hideF3Ip = true;
+				p.useAlias = true;
+				GhostModeConfig.get().save();
+			}
 		});
+
 		LOGGER.info("GhostMode client initialized. Active profile: "
 				+ GhostModeConfig.get().activeProfileName);
 	}
